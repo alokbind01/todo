b5eec77e36a5e22a7418e2b08de73d5ac62a7348
Alok Bind <114241460+alokbind01@users.noreply.github.com>
2025-10-24 21:28:41 +0530

    added undo functionality for completed task

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect,get_object_or_404,HttpResponse
 from .models import Task
 
 def addtask(request):
     task = request.POST['task']
     Task.objects.create(task=task)
+    return redirect('home')
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task,pk=pk)
+    task.is_completed = False
+    task.save()
     return redirect('home')
